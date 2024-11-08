ac9aff89c14339b2874357db866fb775cc2525ed
O4ErtO <vekhin200631@gmail.com>
2024-11-08 13:16:42 +0300

    initial project

---
+++ ProManages/ProManages/ProManagesApp.swift
@@ -0,0 +1,32 @@
+//
+//  ProManagesApp.swift
+//  ProManages
+//
+//  Created by Artem Vekshin on 07.11.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ProManagesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ProManages/ProManagesTests/ProManagesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProManagesTests.swift
+//  ProManagesTests
+//
+//  Created by Artem Vekshin on 07.11.2024.
+//
+
+import Testing
+@testable import ProManages
+
+struct ProManagesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
