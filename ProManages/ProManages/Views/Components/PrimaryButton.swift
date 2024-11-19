c82b211bb582d7882690e80bbc967c118cf7c0fd
O4ErtO <vekhin200631@gmail.com>
2024-11-20 00:40:51 +0300

    setupNavigation

---
+++ ProManages/ProManages/Views/Components/PrimaryButton.swift
@@ -0,0 +1,8 @@
+//
+//  PrimaryButton.swift
+//  ProManages
+//
+//  Created by Artem Vekshin on 18.11.2024.
+//
+
+import Foundation
---
+++ ProManages/ProManages/Views/Components/SecondaryButton.swift
@@ -0,0 +1,8 @@
+//
+//  SecondaryButton.swift
+//  ProManages
+//
+//  Created by Artem Vekshin on 18.11.2024.
+//
+
+import Foundation
